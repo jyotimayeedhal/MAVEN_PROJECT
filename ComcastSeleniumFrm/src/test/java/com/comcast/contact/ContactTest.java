3b6281c12ed6f0323a6c08b8840169169a8e2839
jyotirmayeedhal <jyotirmayeedhal77@gmail.com>
2021-09-21 23:21:46 +0530

    commiting for first time

---
+++ ComcastSeleniumFrm/src/test/java/com/comcast/contact/ContactTest.java
@@ -0,0 +1,20 @@
+package com.comcast.contact;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createContactTest() {
+		System.out.println("exicte ctreateContactTest");
+		
+	}
+	@Test
+	public void createContactWithOrgTest() {
+		System.out.println("execte createContactWithOrgTest");
+	}
+	@Test
+	public void deleteContactTest() {
+		System.out.println("execute deleteContactTest");
+	}
+
+}
